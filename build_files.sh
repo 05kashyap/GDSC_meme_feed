5fdad923ea28fad82601f68dd207f9113de2258c
05kashyap <aryankashyapnaveen@gmail.com>
2023-10-19 19:15:48 +0530

    edit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
 pip install -r requirements.txt 
 python3.9 manage.py collectstatic
-sudo apt-get install pkg-config 
-sudo apt-get install libcairo2-dev libjpeg-dev libgif-dev
+sudo apt-get install pkg-config libcairo2-dev libjpeg-dev libgif-dev
